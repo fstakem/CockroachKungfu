800949f1590d827c1b78b29da110a95b4ff95ea4
Fredrick Stakem <fstakem@fstakem-main.local>
2013-03-10 20:48:49 -0400

    Renamed file.

---
+++ StructuredLog/EventSet.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------
+#
+#   EventSet.py
+#   By: Fred Stakem
+#   Created: 3.10.13
+#
+# ------------------------------------------------------
+
+# Libs
+
+class EventSet(object):
+    """This class represents a set of events that will possibly be in a log."""
+    
+    def __init__(self):
+        pass
+        
